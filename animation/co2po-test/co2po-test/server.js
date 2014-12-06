b32344e42c01fea6ce037ad7360f9e58299a38d5
Martina <petras@re-media.eu>
2014-12-06 19:08:29 +0100

    Added app for testing purposes

---
+++ animation/co2po-test/co2po-test/server.js
@@ -0,0 +1,13 @@
+﻿var http = require('http');
+var express = require('express');
+var app = express();
+
+var port = process.env.port || 1337;
+
+app.use('/', express.static(__dirname + '/index'));
+
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);

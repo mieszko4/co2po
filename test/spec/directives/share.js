786074d73444b110b19124133faf44ff0d548829
Anamarija <kerberina@gmail.com>
2014-12-07 23:28:21 +0100

    Add social share links

---
+++ test/spec/directives/share.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Directive: share', function () {
+
+  // load the directive's module
+  beforeEach(module('co2poApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<share></share>');
+    element = $compile(element)(scope);
+    expect(element.text()).toBe('this is the share directive');
+  }));
+});

959d1f12c0526a7b7ced25bbf9c95977543622f0
Mieszko4 <mieszko4@gmail.com>
2014-12-08 00:48:30 +0100

    Fix scrolling and redoing search

--- app/scripts/directives/statistics.js
+++ app/scripts/directives/statistics.js
@@ -13,34 +13,36 @@ angular.module('co2poApp')
       templateUrl: 'views/directives/statistics.html',
       restrict: 'E',
       link: function (scope) {
-        if (typeof scope.origin === 'undefined') {
-          scope.origin = {
-            lat: 45.374546 + (Math.random() * 5 - 2.5),
-            lng: 15.017002 + (Math.random() * 5 - 2.5)
-          };
-        }
-        
-        if (typeof scope.destination === 'undefined') {
-          scope.destination = {
-            lat: 45.815123 + (Math.random() * 5 - 2.5),
-            lng: 15.981682 + (Math.random() * 5 - 2.5)
-          };
-        }
-        
-        scope.maxCO2 = undefined;
-        scope.distance = undefined;
-        Vehicle.getCO2(scope.origin, scope.destination).then(function (co2s) {
-          scope.co2s = co2s;
+        scope.$watchCollection('[origin, destination]', function () {
+          if (typeof scope.origin === 'undefined') {
+            scope.origin = {
+              lat: 45.374546 + (Math.random() * 5 - 2.5),
+              lng: 15.017002 + (Math.random() * 5 - 2.5)
+            };
+          }
           
-          scope.maxCO2 = 0;
-          angular.forEach(scope.co2s, function (co2) {
-            if (co2.co2 > scope.maxCO2) {
-              scope.maxCO2 = co2.co2;
-              scope.distance = Math.round(co2.distance / 1000);
-            }
+          if (typeof scope.destination === 'undefined') {
+            scope.destination = {
+              lat: 45.815123 + (Math.random() * 5 - 2.5),
+              lng: 15.981682 + (Math.random() * 5 - 2.5)
+            };
+          }
+          
+          scope.maxCO2 = undefined;
+          scope.distance = undefined;
+          Vehicle.getCO2(scope.origin, scope.destination).then(function (co2s) {
+            scope.co2s = co2s;
+            
+            scope.maxCO2 = 0;
+            angular.forEach(scope.co2s, function (co2) {
+              if (co2.co2 > scope.maxCO2) {
+                scope.maxCO2 = co2.co2;
+                scope.distance = Math.round(co2.distance / 1000);
+              }
+            });
+            
+            scope.processCo2s({ co2s: co2s });
           });
-          
-          scope.processCo2s({ co2s: co2s });
         });
       }
     };

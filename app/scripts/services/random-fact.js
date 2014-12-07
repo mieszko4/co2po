99bb9f2cb70d4fc67685fc6b13013b43157227de
Anamarija <kerberina@gmail.com>
2014-12-07 17:15:39 +0100

    Add random-fact retrieval logic

--- app/scripts/services/random-fact.js
+++ app/scripts/services/random-fact.js
@@ -8,12 +8,31 @@
  * Factory in the co2poApp.
  */
 angular.module('co2poApp')
-  .factory('randomFact', function () {
+  .factory('randomFact', function ($resource) {
+    var api = 'https://www.googleapis.com/freebase/v1/';
+
     var service = {
       getFact: function (term) {
-        return 'Random fact for ' + term;
-      }
-    };
-    
-    return service;
+        var searchResource = $resource(api + 'mqlread');
+        return searchResource.get({
+            query: JSON.stringify([{
+                'id': null,
+                'name': null,
+                'name~=': term,
+                '/common/topic/article': [{ 'text': null}]
+            }])
+        }).$promise.then(function (data) {
+                //@todo what to return when nothing found
+            if(data.result.length > 0) {
+                var topicArrayKey = Math.floor(Math.random() * data.result.length);
+                var mid = data.result[topicArrayKey].id;
+
+                var topicResource = $resource(api + 'topic' + mid);
+                return topicResource.get({lang: 'en', limit: '1'}).$promise.then(function (data) {
+                    return data.property['/common/topic/article'].values[0].text;
+                });
+            }
+            });
+    }};
+        return service;
   });

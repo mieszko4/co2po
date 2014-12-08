e0f1c64fe5c2a9049fc9f2c674add68a1a195e13
Mieszko4 <mieszko4@gmail.com>
2014-12-08 06:14:30 +0100

    Fix issue with deploy images

--- Gruntfile.js
+++ Gruntfile.js
@@ -208,6 +208,7 @@ module.exports = function (grunt) {
           '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/**/*.{png,jpg,jpeg,gif,webp,svg}',
+          '!<%= yeoman.dist %>/images/path_icons/*',
           '<%= yeoman.dist %>/styles/fonts/*'
         ]
       }
